5f6851c337d260f00a120b41130b9015f9ba5f39
taylozac85 <taylozac85@gmail.com>
2013-03-14 10:30:12 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brick::Application.config.secret_token = '4ce7d687cd0de4f9f330ba1b76185c03504c7de03c1ef5bda80ca79141b4f104fc5d7680be0e03ecd5950eb73e006371304f8f3562e3e4d0bab6c78bcaee6d6f'
